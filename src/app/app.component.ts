721e0630a7fba4b4c87768141cc7107d848044dc
Gummiees <xavier.caro@koahealth.com>
2023-03-31 11:17:34 +0200

    auto-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,34 @@
-import { Location } from '@angular/common';
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { OverlayService } from '@shared/services/overlay.service';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   public title = 'two-page-app';
   public lang = 'es';
   public overlay$: Observable<boolean>;
-  private subscription?: Subscription;
 
-  constructor(private overlayService: OverlayService, private location: Location) {
+  constructor(private overlayService: OverlayService) {
     this.overlay$ = this.overlayService.overlay$;
     this.lang = this.getCurrentLang();
   }
 
   private getCurrentLang(): string {
-    const path = this.location.path();
+    const paths = location.pathname.replace('/', '').split('/');
+    let path = 'en-US';
+    if (paths.length && paths[0] !== '') {
+      path = paths[0];
+    }
     return path !== '' ? path : 'en-US';
   }
 
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-  }
-
   public onChange() {
-    console.log('onChange', this.lang);
-    this.location.replaceState(this.lang);
+    const url = `${location.origin}/${this.lang}`;
+    console.log('url', url);
+    location.assign(url);
   }
 }
